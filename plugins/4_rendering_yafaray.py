970729f6cf2aa7681e228193b8c23efd25a2feab
povmaniaco <p.alcaide@hotmail.com>
2012-02-22 22:12:27 +0100

    Update options for YafaRay ( originally cloned by Povray files)

--- plugins/4_rendering_yafaray.py
+++ plugins/4_rendering_yafaray.py
@@ -19,48 +19,46 @@ class YafaRayTaskView(gui3d.TaskView):
     def __init__(self, category):
         gui3d.TaskView.__init__(self, category, 'YafaRay')
 
-        optionsBox = self.addView(gui3d.GroupBox([10, 80, 9.0], 'Main Options', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
-        
-        #Buttons
-        renderBox = self.addView(gui3d.GroupBox([10, 380, 9.0], 'Render Options', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
-        
-        source=[]
-        self.iniButton = renderBox.addView(gui3d.RadioButton(source, 'Use render console'))
-        self.guiButton = renderBox.addView(gui3d.RadioButton(source, 'Use render GUI', selected = True))
-        format=[]
-        self.arrayButton = renderBox.addView(gui3d.RadioButton(format, 'XML format'))
-        self.mesh2Button = renderBox.addView(gui3d.RadioButton(format, 'GUI format', selected = True))
-        action=[]
-        self.toExportButton = renderBox.addView(gui3d.RadioButton(action , 'Export to XML', selected = True))
-        self.toRenderButton = renderBox.addView(gui3d.RadioButton(action , 'Export and render'))
-        
-        #self.worldButton = optionsBox.addView(gui3d.Button('World'))
+        # Buttons
+        posy = 80
+        worldBox = self.addView(gui3d.GroupBox([10, posy, 9.0], 'Background', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
+        world=[]
+        self.sunskyButton = worldBox.addView(gui3d.RadioButton(world, 'SunSky'))
+        self.texButton = worldBox.addView(gui3d.RadioButton(world, 'Texture', selected = True))
+        self.colButton = worldBox.addView(gui3d.RadioButton(world, 'Color'))
         #
-        self.renderButton = renderBox.addView(gui3d.Button('Render GUI'))
-        #self.xmlButton = renderBox.addView(gui3d.Button('Export XML'))
-        #
-        lightingBox = self.addView(gui3d.GroupBox([140, 380, 9.0], 'Integrators', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
+        lightingBox = self.addView(gui3d.GroupBox([10, 190, 9.0], 'Integrators', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
         lighting = []
         self.dlButton = lightingBox.addView(gui3d.RadioButton(lighting, 'DirectLighting', selected = True))
         self.pmButton = lightingBox.addView(gui3d.RadioButton(lighting, 'Photon Map'))
-        self.ptButton = lightingBox.addView(gui3d.RadioButton(lighting, 'Pathtracing'))
+        #
+        renderBox = self.addView(gui3d.GroupBox([10, 275, 9.0], 'Render Options', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
+        source=[]
+        self.consoleButton = renderBox.addView(gui3d.RadioButton(source, 'Render console'))
+        self.guiButton = renderBox.addView(gui3d.RadioButton(source, 'Render GUI', selected = True))
+        self.xmlButton = renderBox.addView(gui3d.RadioButton(source, 'Write XML'))
+        self.renderButton = renderBox.addView(gui3d.Button('Render'))
+        #
         
-        worldBox = self.addView(gui3d.GroupBox([140, 80, 9.0], 'Background', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
-        world=[]
-        self.sunskyButton = worldBox.addView(gui3d.RadioButton(world, 'SunSky'))
-        self.texButton = worldBox.addView(gui3d.RadioButton(world, 'Texture'))
-        self.colButton = worldBox.addView(gui3d.RadioButton(world, 'Color', selected = True))
-              
+        '''
+        optionsBox = self.addView(gui3d.GroupBox([10, 420, 9.0], 'Main Options', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
+        options = []
+        self.raydButton = optionsBox.addView(gui3d.RadioButton(options, 'RayDepth'))
+        self.zbufButton = optionsBox.addView(gui3d.RadioButton(options, 'Z Buffer'))
+        #
+        #aaBox = self.addView(gui3d.GroupBox([10, 190, 9.0], 'Antialias', gui3d.GroupBoxStyle._replace(height=25+24*7+6)))
+        '''        
+                      
 
         @self.renderButton.event
         def onClicked(event):            
             
             reload(mh2yafaray)  # Avoid having to close and reopen MH for every coding change (can be removed once testing is complete)
             mh2yafaray.yafarayRenderer(gui3d.app.selectedHuman.mesh, gui3d.app,
-                {'source':'ini' if self.iniButton.selected else 'gui',
-                 'format':'array' if self.arrayButton.selected else 'mesh2',
-                 'action':'export' if self.toExportButton.selected else 'render',
-                 'world':'sunsky' if self.sunskyButton.selected else 'color'})
+                {'source':'console' if self.consoleButton.selected else 'gui' if self.guiButton.selected else 'xml',
+                 'format':'xml' if self.xmlButton.selected else 'gui',
+                 'lighting':'dl' if self.dlButton.selected else 'pm',
+                 'world':'sunsky' if self.sunskyButton.selected else 'texture' if self.texButton.selected else 'color'})
 
     def onShow(self, event):
         self.renderButton.setFocus()
